a216889503717406ad6191a4af94839a3c036f7c
ftsofialeivaa-lgtm <ftsofialeivaa@gmail.com>
2026-05-12 22:53:21 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,11 @@
 <?php
-$host     = "localhost";
+$host     = "postgres.railway.internal";
 $port     = "5432";
-$dbname   = "inventario_db";
+$dbname   = "railway";
 $user     = "postgres";
-$password = "47460"; // 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$password = "txccDihzEGeuqPYsPIYTVtfJOxCeKkWX";
+
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
 if (!$conn) {
     die("Error: No se pudo conectar a la base de datos.");
