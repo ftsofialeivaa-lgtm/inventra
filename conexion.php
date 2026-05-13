ef579b6f7752efc11dbfb7238695879cc845c218
ftsofialeivaa-lgtm <ftsofialeivaa@gmail.com>
2026-05-12 23:03:04 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,11 @@
 <?php
-$host     = "postgres.railway.internal";
-$port     = "5432";
-$dbname   = "railway";
-$user     = "postgres";
-$password = "txccDihzEGeuqPYsPIYTVtfJOxCeKkWX";
+$url = parse_url("postgresql://postgres:txccDihzEGeuqPYsPIYTVtfJOxCeKkWX@shuttle.proxy.rlwy.net:16824/railway");
+
+$host     = $url["host"];
+$port     = $url["port"];
+$dbname   = ltrim($url["path"], "/");
+$user     = $url["user"];
+$password = $url["pass"];
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
